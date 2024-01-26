2cc2bbefb554d26a9b55fb29b94f59f6cbce0fa1
Code In Ink <CodeInInk@gmail.com>
2024-01-26 14:02:03 -0800

    Rough draft on background.

---
+++ source/Tile.hx
@@ -0,0 +1,23 @@
+
+
+class Tile {
+	enum GenType {
+		Ocean;
+		Random;
+	}
+
+	public function new() {
+	
+	}
+
+	static public function Generate(genType:GenType) {
+	
+		switch genType {
+			case GenType.Ocean:
+			
+			case GenType.Random
+		}
+	
+		return Tile.new();
+	}
+}
