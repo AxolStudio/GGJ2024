96c199dd547da04bd9f59286c54ee03796246f85
Code In Ink <CodeInInk@gmail.com>
2024-01-27 12:41:25 -0800

    Make coins seek player.

--- source/objects/Coin.hx
+++ source/objects/Coin.hx
@@ -1,10 +1,24 @@
 package objects;
 
+import flixel.tweens.FlxTween;
+
+enum CoinState
+{
+	Idle;
+	Seeking;
+	Acquired;
+}
+
 class Coin extends FlxTypedSpriteGroup<FlxSprite>
 {
 	
 	public var sprite:FlxSprite;
 	public var value:Int;
+	
+	public var state:CoinState = CoinState.Idle;
+	
+	public var seekTimeMS = 50;
+	public var seekRange  = 100;
 	
 	public function new(x:Float, y:Float):Void
 	{
@@ -49,5 +63,67 @@ class Coin extends FlxTypedSpriteGroup<FlxSprite>
 		newCoin.add(newCoin.sprite);
 		
 		return newCoin;
+	}
+	
+	
+	public function IsPlayerInSight():Bool
+	{
+		return FlxMath.distanceBetween(this, Globals.PlayState.player) < seekRange;
+	}
+	
+	public function SetState(newState:CoinState)
+	{
+		state = newState;
+		
+		switch(state)
+		{
+			case CoinState.Idle:
+				null;
+				
+			case CoinState.Seeking:
+				null;
+				
+			case CoinState.Acquired:
+				Globals.PlayState.player.wallet.AddCoin(this);
+				FlxTween.tween(this,{alpha:0},.2, {onComplete:(_)->{this.kill();}});
+		}
+	}
+	
+	public function HandleIdle()
+	{
+		if (IsPlayerInSight())
+		{
+			SetState(CoinState.Seeking);
+		}
+	}
+	
+	public function HandleSeeking()
+	{
+		FlxVelocity.moveTowardsObject(this, Globals.PlayState.player, 0, seekTimeMS);
+		if (FlxMath.distanceBetween(this, Globals.PlayState.player) < 10)
+		{
+			SetState(CoinState.Acquired);
+		}
+	}
+	
+	public function ProcessState()
+	{
+		switch(state)
+		{
+			case CoinState.Idle:
+				HandleIdle();
+				
+			case CoinState.Seeking:
+				HandleSeeking();
+				
+			case CoinState.Acquired:
+				null;
+		}
+	}
+	
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		ProcessState();
 	}
 }
