e929ad2f7f820ec68d44911ec9b97da253c46b27
Tim I Hely <seifertim@gmail.com>
2024-01-26 18:30:24 -0600

    working on seperate pieces for ship

--- source/objects/Player.hx
+++ source/objects/Player.hx
@@ -1,20 +1,53 @@
 package objects;
 
+import flixel.group.FlxSpriteGroup;
 import flixel.math.FlxAngle;
 import flixel.math.FlxMath;
 import flixel.math.FlxVelocity;
 
-class Player extends FlxSprite
+class Player extends FlxTypedSpriteGroup<FlxSprite>
 {
 	public static inline var SPEED_PER_RANK:Int = 100;
 
 	public var speedLevel:Int = 0;
 	public var speedChangeDelay:Float = -1;
 
+	public var hull:FlxSprite;
+	public var sail:FlxSprite;
+	public var nest:FlxSprite;
+
+	// public var cannons:Array<FlxSprite>; // eventually
+
 	public function new():Void
 	{
 		super();
-		loadGraphic(GraphicsCache.loadGraphic("assets/images/player-ship.png"), false, 113, 66, false, "player-ship");
+		hull = new FlxSprite();
+		hull.loadGraphic(GraphicsCache.loadGraphic("assets/images/ship-base.png"), false, 108, 40, false, "ship-base");
+		sail = new FlxSprite();
+		sail.loadGraphic(GraphicsCache.loadGraphic("assets/images/sail.png"), true, 46, 66, false, "sail");
+		nest = new FlxSprite();
+		nest.loadGraphic(GraphicsCache.loadGraphic("assets/images/ship-nest.png"), false, 18, 20, false, "ship-nest");
+
+		hull.origin.x = 44;
+		hull.origin.y = 19;
+
+		sail.origin.x = 35;
+		sail.origin.y = 33;
+
+		nest.origin.x = 49;
+		nest.origin.y = 10;
+
+		sail.x = hull.x + 9;
+		sail.y = hull.y + (hull.height / 2) - (sail.height / 2);
+
+		nest.x = hull.x - 5;
+		nest.y = hull.y + (hull.height / 2) - (nest.height / 2);
+
+		add(hull);
+		add(sail);
+		add(nest);
+
+		sail.animation.frameIndex = 3;
 
 		maxVelocity.x = maxVelocity.y = SPEED_PER_RANK * 3;
 		drag.x = drag.y = 100;
@@ -53,11 +86,13 @@ class Player extends FlxSprite
 			{
 				speedLevel++;
 				speedChangeDelay = 0.1;
+				sail.animation.frameIndex = Std.int(Math.min(3, 3 - speedLevel));
 			}
 			else if (down && speedLevel > -1)
 			{
 				speedLevel--;
 				speedChangeDelay = 0.1;
+				sail.animation.frameIndex = Std.int(Math.min(3, 3 - speedLevel));
 			}
 		}
 
