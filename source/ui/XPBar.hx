2023080e5a5662283a7ac2093cd780ed34a58711
Code In Ink <CodeInInk@gmail.com>
2024-01-28 12:36:30 -0800

    Add titles to the player's level.

--- source/ui/XPBar.hx
+++ source/ui/XPBar.hx
@@ -10,6 +10,7 @@ class XPBar extends FlxTypedGroup<FlxSprite>
 	var currentLevel:Int = 1;
 	
 	public var playerLevelText:GameText;
+	public var playerLevelTitle:GameText;
 	
 	public function new():Void
 	{
@@ -19,11 +20,17 @@ class XPBar extends FlxTypedGroup<FlxSprite>
 		
 		playerLevelText = new GameText("1");
 		playerLevelText.alignment = "center";
-		playerLevelText.x = FlxG.width/2+180 - playerLevelText.width / 2;
-		playerLevelText.y = 25+25+4;
+		playerLevelText.x  = FlxG.width/2+180 - playerLevelText.width / 2;
+		playerLevelText.y  = 25+25+4;
+		
+		playerLevelTitle = new GameText(GetRankText());
+		playerLevelTitle.alignment = "center";
+		playerLevelTitle.x = FlxG.width/2 - playerLevelTitle.width / 2;
+		playerLevelTitle.y = FlxG.height - 40;
 		
 		add(_bar);
 		add(playerLevelText);
+		add(playerLevelTitle);
 		
 		forEach(function(sprite) sprite.scrollFactor.set(0, 0));
 	}
@@ -63,5 +70,58 @@ class XPBar extends FlxTypedGroup<FlxSprite>
 		
 		playerLevelText.text = Std.string(currentLevel);
 		playerLevelText.x = FlxG.width/2+180 - playerLevelText.width / 2;
+		
+		playerLevelTitle.text = GetRankText();
+		playerLevelTitle.alignment = "center";
+		playerLevelTitle.x = FlxG.width/2 - playerLevelTitle.width / 2;
+	}
+	
+	public function GetRankText():String
+	{
+		switch (currentLevel)
+		{
+			case 1:
+				return "Juvenile Jokester";
+				
+			//case 2:
+			//	return "Juvenile Jokester II";
+			//	
+			//case 3:
+			//	return "Juvenile Jokester III";
+				
+				
+			case 2:
+				return "Apprentice Juggler";
+				
+			//case 5:
+			//	return "Apprentice Juggler II";
+			//	
+			//case 6:
+			//	return "Apprentice Juggler III";
+				
+				
+			case 3:
+				return "Adept Pie Thrower";
+				
+			//case 8:
+			//	return "Expert Pie Thrower II";
+			//	
+			//case 9:
+			//	return "Expert Pie Thrower III";
+				
+				
+			case 4:
+				return "Expert Clown";
+				
+			//case 11:
+			//	return "Master Clown II";
+			//	
+			//case 12:
+			//	return "Master Clown III";
+			//	
+			case _:
+				return "Endless Prankster";
+
+		}
 	}
 }
