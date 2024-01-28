0337863e3fd52c9fed7b9afd282e81c641618c01
Code In Ink <CodeInInk@gmail.com>
2024-01-28 11:59:36 -0800

    Add Player Level text.

--- source/ui/XPBar.hx
+++ source/ui/XPBar.hx
@@ -1,6 +1,7 @@
 package ui;
 
 import flixel.ui.FlxBar;
+import ui.GameText;
 
 class XPBar extends FlxTypedGroup<FlxSprite>
 {
@@ -8,13 +9,21 @@ class XPBar extends FlxTypedGroup<FlxSprite>
 	var currentXP:Int = 0;
 	var currentLevel:Int = 1;
 	
+	public var playerLevelText:GameText;
+	
 	public function new():Void
 	{
 		super();
 		
 		_bar = new FlxBar(FlxG.width/2+80, 25, LEFT_TO_RIGHT, 200, 25, null, "", 0, 100, true);
 		
+		playerLevelText = new GameText("1");
+		playerLevelText.alignment = "center";
+		playerLevelText.x = FlxG.width/2+180 - playerLevelText.width / 2;
+		playerLevelText.y = 25+25+4;
+		
 		add(_bar);
+		add(playerLevelText);
 		
 		forEach(function(sprite) sprite.scrollFactor.set(0, 0));
 	}
@@ -51,5 +60,8 @@ class XPBar extends FlxTypedGroup<FlxSprite>
 		currentXP -= XPNeeded();
 		
 		currentLevel += 1;
+		
+		playerLevelText.text = Std.string(currentLevel);
+		playerLevelText.x = FlxG.width/2+180 - playerLevelText.width / 2;
 	}
 }
